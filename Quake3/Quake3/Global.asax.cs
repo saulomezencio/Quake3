d5ba81418c1966d214b4c643ad1ce1971b549724
Saulo Mezêncio da Silva <saulomezencio@gmail.com>
2018-10-25 20:52:56 -0300

    Alterando o Global.asax.cs para poder abrir o json em navegador;

--- Quake3/Quake3/Global.asax.cs
+++ Quake3/Quake3/Global.asax.cs
@@ -12,6 +12,8 @@ namespace Quake3
         protected void Application_Start()
         {
             GlobalConfiguration.Configure(WebApiConfig.Register);
+            GlobalConfiguration.Configuration.Formatters.JsonFormatter.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
+            GlobalConfiguration.Configuration.Formatters.Remove(GlobalConfiguration.Configuration.Formatters.XmlFormatter);
         }
     }
 }
